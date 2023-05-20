728909d09af9dd04074f699274a47f325caf93bb
Carlos Alberto <cgomespimentel7@gmail.com>
2023-05-20 12:54:09 -0300

    feat: Implementação de cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,14 @@
 
 const express = require('express');
 const router = require('./router');
-
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
+
 
 module.exports = app;
 
